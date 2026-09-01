6a8d89af0583f1177b34668256ffd073dc682eb2
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 11:53:22 +0530

    style(ui): align generated specs with Prettier output

--- src/test.ts
+++ src/test.ts
@@ -2,10 +2,10 @@ import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserTestingModule,
-  platformBrowserTesting,
+  platformBrowserTesting
 } from '@angular/platform-browser/testing';
 
 getTestBed().initTestEnvironment(
   BrowserTestingModule,
-  platformBrowserTesting(),
+  platformBrowserTesting()
 );
