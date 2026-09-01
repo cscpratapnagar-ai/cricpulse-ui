6c79c5a5fc1e1bccf58301143543b6b0cf386ae3
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 09:08:30 +0530

    test(angular): add component smoke spec

---
+++ src/app/features/teams/pages/create-team/create-team.component.spec.ts
@@ -0,0 +1,2 @@
+import { CreateTeamComponent } from './create-team.component';
+describe('CreateTeamComponent',()=>{it('should expose the component class',()=>expect(CreateTeamComponent).toBeTruthy());});
