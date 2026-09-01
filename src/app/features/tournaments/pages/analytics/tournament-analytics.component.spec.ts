b14147dd6a8be39859919f2cecf2a7f1ac3581cb
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 09:19:54 +0530

    test(angular): add tournament analytics smoke spec

---
+++ src/app/features/tournaments/pages/analytics/tournament-analytics.component.spec.ts
@@ -0,0 +1,2 @@
+import { TournamentAnalyticsComponent } from './tournament-analytics.component';
+describe('TournamentAnalyticsComponent',()=>{it('should expose the component class',()=>expect(TournamentAnalyticsComponent).toBeTruthy());});
