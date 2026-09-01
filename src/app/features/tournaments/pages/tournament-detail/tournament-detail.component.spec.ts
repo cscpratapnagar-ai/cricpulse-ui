fac182277b5fd1193e547fc1e73a5441247987e0
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 09:10:18 +0530

    test(angular): add component smoke spec

---
+++ src/app/features/tournaments/pages/tournament-detail/tournament-detail.component.spec.ts
@@ -0,0 +1,2 @@
+import { TournamentDetailComponent } from './tournament-detail.component';
+describe('TournamentDetailComponent',()=>{it('should expose the component class',()=>expect(TournamentDetailComponent).toBeTruthy());});
