d69bb9dfca86b543801101ebf2b60daf3da07679
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 09:08:17 +0530

    test(angular): add component smoke spec

---
+++ src/app/features/teams/pages/bulk-players/bulk-team-players.component.spec.ts
@@ -0,0 +1,2 @@
+import { BulkTeamPlayersComponent } from './bulk-team-players.component';
+describe('BulkTeamPlayersComponent',()=>{it('should expose the component class',()=>expect(BulkTeamPlayersComponent).toBeTruthy());});
