75e4cd8d731672de1d7c85e2207e3d50a5a53963
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 09:09:54 +0530

    test(angular): add component smoke spec

---
+++ src/app/features/tournaments/pages/qualification/tournament-qualification.component.spec.ts
@@ -0,0 +1,2 @@
+import { TournamentQualificationComponent } from './tournament-qualification.component';
+describe('TournamentQualificationComponent',()=>{it('should expose the component class',()=>expect(TournamentQualificationComponent).toBeTruthy());});
