190501f9f3f3633f5a34387bb4b3fd8c26dfa7e5
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 11:57:06 +0530

    style(ui): apply exact Prettier output from CI

--- src/test.ts
+++ src/test.ts
@@ -1,11 +1,5 @@
 import 'zone.js/testing';
 import { getTestBed } from '@angular/core/testing';
-import {
-  BrowserTestingModule,
-  platformBrowserTesting
-} from '@angular/platform-browser/testing';
+import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
 
-getTestBed().initTestEnvironment(
-  BrowserTestingModule,
-  platformBrowserTesting()
-);
+getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
