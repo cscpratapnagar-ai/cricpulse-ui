3755a55fc8e02f4f5807a722155db42c8b1cf445
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 09:06:31 +0530

    test(angular): add component smoke spec

---
+++ src/app/features/scoring/pages/scorecard/scorecard.component.spec.ts
@@ -0,0 +1,2 @@
+import { ScorecardComponent } from './scorecard.component';
+describe('ScorecardComponent',()=>{it('should expose the component class',()=>expect(ScorecardComponent).toBeTruthy());});
