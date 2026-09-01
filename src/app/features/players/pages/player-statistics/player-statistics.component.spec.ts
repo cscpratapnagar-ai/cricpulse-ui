a90b818f734ba99eb76045294913f0b76af48f26
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-01 09:05:22 +0530

    test(angular): add component smoke spec

---
+++ src/app/features/players/pages/player-statistics/player-statistics.component.spec.ts
@@ -0,0 +1,2 @@
+import { PlayerStatisticsComponent } from './player-statistics.component';
+describe('PlayerStatisticsComponent',()=>{it('should expose the component class',()=>expect(PlayerStatisticsComponent).toBeTruthy());});
