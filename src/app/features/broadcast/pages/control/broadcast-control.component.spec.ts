ff8ebfc70c6dbc50c6d298d82d1aa1b60d7e7425
cscpratapnagar-ai <cscpratapnagar@gmail.com>
2026-09-02 12:43:55 +0530

    test(broadcast): add control component spec

---
+++ src/app/features/broadcast/pages/control/broadcast-control.component.spec.ts
@@ -0,0 +1,5 @@
+import { BroadcastControlComponent } from './broadcast-control.component';
+
+describe('BroadcastControlComponent', () => {
+  it('should expose the component class', () => expect(BroadcastControlComponent).toBeTruthy());
+});
